681bd1b4ad5549f2b97ef4c3f92adf5350739792
Chiqun Zhang <chizhang@microsoft.com>
2020-05-21 10:08:03 -0700

    debug

--- ch12/dense_mono/dense_mapping.cpp
+++ ch12/dense_mono/dense_mapping.cpp
@@ -167,9 +167,9 @@ int main(int argc, char **argv) {
 
         imwrite(std::string(argv[1]) + "/depth/depth_" + std::to_string(img_i) + ".png", depth/init_depth*255);
         color_image_files.push_back(color_image_files.front());
-        color_image_files.erase(color_image_files.front());
+        color_image_files.erase(color_image_files.begin());
         poses_TWC.push_back(poses_TWC.front());
-        poses_TWC.erase(poses_TWC.front());
+        poses_TWC.erase(poses_TWC.begin());
     }
    
     cout << "done." << endl;
